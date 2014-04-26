77a4c744592c5e0b6152f3f78e8db98c93fc684a
Jonas Baumann <jone@jone.ch>
2014-04-26 17:07:12 +0200

    Create package.

---
+++ README.rst
@@ -0,0 +1,24 @@
+==========================
+ ftw.recipe.checkversions
+==========================
+
+The ``ftw.recipe.checkversions`` buildout recipe helps you finding newer versions
+of your dependency packages.
+It does this based on your version pinnings (buildout config file) by checking the
+current releases of each package on pypi.
+
+Links
+=====
+
+- github project: https://github.com/4teamwork/ftw.recipe.checkversions
+- Issue tracker: https://github.com/4teamwork/ftw.recipe.checkversions/issues
+- Package on pypi: http://pypi.python.org/pypi/ftw.recipe.checkversions
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.recipe.checkversions
+
+
+Copyright
+=========
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.recipe.checkversions`` is licensed under GNU General Public License, version 2.
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '1.0.0.dev0'
+
+
+tests_require = [
+    'unittest2',
+    ]
+
+
+setup(name='ftw.recipe.checkversions',
+      version=version,
+      description='Buildout recipe for finding new package versions.',
+
+      long_description=open('README.rst').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+
+      keywords='ftw recipe checkversions buildout',
+      author='4teamwork AG',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.recipe.checkversions',
+
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', 'ftw.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        ],
+
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points = {
+        },
+      )
