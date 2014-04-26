14d4d0ffa8a81496865f984d5857d73f7e5c0e95
Jonas Baumann <jone@jone.ch>
2014-04-26 21:43:19 +0200

    Implement checkeversions command.

--- ftw/recipe/checkversions/command.py
+++ ftw/recipe/checkversions/command.py
@@ -1,3 +1,14 @@
+from ftw.recipe.checkversions.buildout import read_versions
+from ftw.recipe.checkversions.checker import get_version_updates
 
-def main(*args, **kwargs):
-    print 'Hello World', args, kwargs
+
+def main(buildout_dir, versions, blacklists):
+    current_versions = read_versions(buildout_dir, versions)
+    blacklist = dict()
+    for file_or_url in blacklists:
+        blacklist.update(read_versions(buildout_dir, file_or_url))
+
+    updates = get_version_updates(current_versions, blacklist.keys())
+
+    for package, version in sorted(updates.items()):
+        print package, '=', version
